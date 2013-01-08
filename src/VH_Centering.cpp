dc622c68d9d40e85b72681cfbe0ffa83e7d56164
Vilya Harvey <vilya.harvey@gmail.com>
2013-01-08 14:37:26 +0000

    Make VH_Centering do what it says on the tin

--- src/VH_Centering.cpp
+++ src/VH_Centering.cpp
@@ -12,7 +12,7 @@ namespace vh {
   //
 
   static const char* kPluginClass = "VH_Centering";
-  static const char* kPluginHelp = "Apply simple rules to particles, to make them behave like a flock.";
+  static const char* kPluginHelp = "Particles will try to move towards the centre of the flock.";
 
 
   //
@@ -37,40 +37,10 @@ namespace vh {
     static const Description desc;
 
   private:
-    void flocking(const ParticleContext& context, ParticleSystem* ps);
-
-    Vector3 centering(const Vector3& position,
-                      const Vector3& summedPositions,
-                      const int numParticles) const;
-
-    Vector3 separation(const Vector3& position,
-                       const ParticleSystem* ps,
-                       const unsigned int i,
-                       const int numParticles,
-                       const float separationSquared) const;
-
-    Vector3 matchVelocity(const Vector3& velocity,
-                          const Vector3& summedVelocities,
-                          const int numParticles) const;
-
-    Vector3 goalSeeking(const Vector3& position) const;
-
-    Vector3 avoidance(const Vector3& position,
-                      const float avoidDistanceSquared) const;
-
-    Vector3 speedLimit(const Vector3& force,
-                       const float maxSpeedSquared) const;
+    void centering(const ParticleContext& context, ParticleSystem* ps);
 
   private:
     float _centeringRate;
-    float _separation;
-    float _velocityMatchRate;
-    float _goalAttainRate;
-    float _maxSpeed;
-    Vector3 _goal;
-    Vector3 _avoid;
-    bool _avoidEnabled;
-    float _avoidDistance;
   };
 
 
@@ -80,15 +50,7 @@ namespace vh {
 
   VH_Centering::VH_Centering(Node* node) :
     ParticleBehaviour(node),
-    _centeringRate(100.0f),
-    _separation(0.1f),
-    _velocityMatchRate(8.0f),
-    _goalAttainRate(100.0f),
-    _maxSpeed(1.0f),
-    _goal(0.0f, 0.0f, 0.0f),
-    _avoid(0.0f, 0.0f, 0.0f),
-    _avoidEnabled(false),
-    _avoidDistance(1.0f)
+    _centeringRate(100.0f)
   {
   }
 
@@ -97,49 +59,36 @@ namespace vh {
   {
     ParticleBehaviour::knobs(f);
     Float_knob(f, &_centeringRate, "centering_rate", "centering rate");
-    Float_knob(f, &_separation, "separation_distance", "separation distance");
-    Float_knob(f, &_velocityMatchRate, "velocity_match_rate", "velocity match rate");
-    Float_knob(f, &_goalAttainRate, "goal_attainment_rate", "goal attainment rate");
-    Float_knob(f, &_maxSpeed, "maximum_speed", "maximum speed");
-    XYZ_knob(f, &_goal.x, "goal");
-
-    XYZ_knob(f, &_avoid.x, "avoid");
-    Bool_knob(f, &_avoidEnabled, "avoid_enabled", "enable");
-    Float_knob(f, &_avoidDistance, "avoid_distance", "avoid distance");
   }
 
 
 #if NUKE_VERSION_MAJOR >= 7
   bool VH_Centering::applyBehaviour(const ParticleContext& context, ParticleSystem* ps)
   {
-    flocking(context, ps);
+    centering(context, ps);
     return true;
   }
 #else
   void VH_Centering::applyBehaviour(const ParticleContext& context, ParticleSystem* ps)
   {
-    flocking(context, ps);
+    centering(context, ps);
   }
 #endif
 
 
-  void VH_Centering::flocking(const ParticleContext& context, ParticleSystem* ps)
+  void VH_Centering::centering(const ParticleContext& context, ParticleSystem* ps)
   {
     const unsigned int kNumParticles = ps->numParticles();
-    const float kSeparationSquared = _separation * _separation;
-    const float kMaxSpeedSquared = _maxSpeed * _maxSpeed;
-    const float kAvoidDistanceSquared = _avoidDistance * _avoidDistance;
     const double kEnd = context.endTime();
 
     // Pre-calculations
     Vector3 summedPositions(0, 0, 0);
-    Vector3 summedVelocities(0, 0, 0);
     for (unsigned int i = 0; i < kNumParticles; ++i) {
       summedPositions += ps->particlePosition(i);
-      summedVelocities += ps->particleVelocity(i);
     }
 
     // Apply the flocking rules to each particle.
+    const unsigned int kNumOtherParticles = (kNumParticles > 1) ? (kNumParticles - 1) : 1;
     for (unsigned int i = 0; i < kNumParticles; ++i) {
       double startTime = ps->particleStartTime(i);
       if (startTime > kEnd)
@@ -150,112 +99,11 @@ namespace vh {
         dt = context.dt();
 
       Vector3 position = ps->particlePosition(i);
-      Vector3 velocity = ps->particleVelocity(i);
-      Vector3 force = -velocity;
-
-      // Rule 1: particles try to move towards the centre of the flock.
-      force += centering(position, summedPositions, kNumParticles);
-
-      // Rule 2: particles try to maintain a minimum separation from each other.
-      force += separation(position, ps, i, kNumParticles, kSeparationSquared);
-
-      // Rule 3: particles try to match velocity with each other.
-      force += matchVelocity(velocity, summedVelocities, kNumParticles);
-
-      // Rule 4: particles try to move towards a common goal.
-      force += goalSeeking(position);
-
-      // Rule 5: avoid a specified location.
-      if (_avoidEnabled)
-        force += avoidance(position, kAvoidDistanceSquared);
-
-      // Rule 6: limit the maximum speed of movement.
-      force = speedLimit(force, kMaxSpeedSquared);
+      Vector3 perceivedCenter = (summedPositions - position) / kNumOtherParticles;
+      Vector3 motionTowardsCenter = (perceivedCenter - position) / _centeringRate;
 
       // Apply the total force to the particle.
-      applyForce(ps, i, context, force);
-    }
-  }
-
-
-  Vector3 VH_Centering::centering(const Vector3& position,
-                                 const Vector3& summedPositions,
-                                 const int numParticles) const
-  {
-    Vector3 perceivedCenter = (summedPositions - position) / (numParticles - 1);
-    Vector3 motionTowardsCenter = (perceivedCenter - position) / _centeringRate;
-    return motionTowardsCenter;
-  }
-
-
-  Vector3 VH_Centering::separation(const Vector3& position,
-                                  const ParticleSystem* ps,
-                                  const unsigned int i,
-                                  const int numParticles,
-                                  const float separationSquared) const
-  {
-    Vector3 motionAwayFromOthers(0, 0, 0);
-    for (unsigned int j = 0; j < numParticles; ++j) {
-      if (j == i)
-        continue;
-
-      Vector3 gap = ps->particlePosition(j) - position;
-      float distanceSquared = gap.lengthSquared();
-      if (distanceSquared < separationSquared)
-        motionAwayFromOthers -= gap;
-    }
-    return motionAwayFromOthers;
-  }
-
-
-  Vector3 VH_Centering::matchVelocity(const Vector3& velocity,
-                                     const Vector3& summedVelocities,
-                                     const int numParticles) const
-  {
-    Vector3 perceivedVelocity =
-      (summedVelocities - velocity) / (numParticles - 1);
-    Vector3 motionWithFlock =
-      (perceivedVelocity - velocity) / _velocityMatchRate;
-    return motionWithFlock;
-  }
-
-
-  Vector3 VH_Centering::goalSeeking(const Vector3& position) const
-  {
-    Vector3 motionTowardsGoal = (_goal - position) / _goalAttainRate;
-    return motionTowardsGoal;
-  }
-
-
-  Vector3 VH_Centering::avoidance(const Vector3& position,
-                                 const float avoidDistanceSquared) const
-  {
-    Vector3 gap = position - _avoid;
-    float distanceFromAvoidSquared = gap.lengthSquared();
-    if (distanceFromAvoidSquared < avoidDistanceSquared) {
-      float distanceFromAvoid = sqrtf(distanceFromAvoidSquared);
-      Vector3 normal = gap / distanceFromAvoid;
-
-      float distanceInside = _avoidDistance - distanceFromAvoid;
-      return normal * distanceInside;
-    }
-    else {
-      return Vector3(0, 0, 0);
-    }
-  }
-
-
-  Vector3 VH_Centering::speedLimit(const Vector3& force,
-                                  const float maxSpeedSquared) const
-  {
-    float speedSquared = force.lengthSquared();
-    if (speedSquared > maxSpeedSquared) {
-      float speed = sqrtf(speedSquared);
-      Vector3 newForce = force / speed * _maxSpeed;
-      return newForce;
-    }
-    else {
-      return force;
+      applyForce(ps, i, context, motionTowardsCenter);
     }
   }
 
